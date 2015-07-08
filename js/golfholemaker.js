c01e8d9f8efe243270d2b0d00d5db754a2deaf00
Jonathan Sutcliffe <jonathan.a.sutcliffe@gmail.com>
2015-07-08 13:52:49 +0100

    Add a smidge of commenting

--- js/golfholemaker.js
+++ js/golfholemaker.js
@@ -10,23 +10,33 @@ var golfHoleMaker = {
     templates: {
         colorPicker: '' +
             '<ul>' +
-                '<% for(var i = 0, il = colors.length; i < il; i++) { %>' +
-                    '<li>' +
-                        '<label for="color-<%= colors[i].hex.replace("#", "") %>" style="background-color: <%= colors[i].hex %>">COLOR</label>' +
-                        '<input type="text" length="1" id="color-<%= colors[i].hex.replace("#", "") %>"/>' +
-                    '</li>\n' +
-                '<% } %>' +
+            '<% for(var i = 0, il = colors.length; i < il; i++) { %>' +
+            '<li>' +
+            '<label for="color-<%= colors[i].hex.replace("#", "") %>" style="background-color: <%= colors[i].hex %>">COLOR</label>' +
+            '<input type="text" length="1" id="color-<%= colors[i].hex.replace("#", "") %>"/>' +
+            '</li>\n' +
+            '<% } %>' +
             '</ul>'
     },
 
+    /**
+     * Set up annecessary bits and pieces
+     */
     init: function () {
         this.bindEvents();
     },
 
+    /**
+     * Add event listeners to the tool
+     */
     bindEvents: function () {
         document.getElementById('file').addEventListener('change', this.handleFileSelect, true);
     },
 
+    /**
+     * Handles image loading and passes the result to drawImage()
+     * @param event
+     */
     handleFileSelect: function (event) {
         var file = event.target.files[0];
 
@@ -47,6 +57,10 @@ var golfHoleMaker = {
         reader.readAsDataURL(file);
     },
 
+    /**
+     * Draws an image onto a canvas and kicks off the processImage method
+     * @param image
+     */
     drawImage: function (image) {
         var canvas = this.settings.holeCanvas;
         var context = canvas.getContext('2d');
@@ -54,7 +68,7 @@ var golfHoleMaker = {
         if(image) {
             if(image.width <= this.settings.imageSizeThreshold && image.height <= this.settings.imageSizeThreshold)
 
-            context.drawImage(image, 0, 0, image.width, image.height);
+                context.drawImage(image, 0, 0, image.width, image.height);
 
             golfHoleMaker.processImage(context.getImageData(0, 0, image.width, image.height));
         } else {
@@ -62,6 +76,10 @@ var golfHoleMaker = {
         }
     },
 
+    /**
+     * Creates an array of color values from the pixels in the image
+     * @param imageData
+     */
     processImage: function (imageData) {
         var valueArray = [];
 
@@ -76,12 +94,16 @@ var golfHoleMaker = {
         this.createColorSelect(valueArray);
     },
 
+    /**
+     * Populate a set of inputs used to specify what type of tile to draw for each color in the processed image
+     * @param valueArray
+     */
     createColorSelect: function (valueArray) {
-        // 1) convert the hexes into single characters
-        var allColors = _.unique(valueArray);
-
         if (allColors.length <= this.settings.colorCountThreshold) {
+            // reduce the array to unique values to determine how many colors we are dealing with
+            var allColors = _.unique(valueArray);
 
+            // transform allColors into an array of objects for greater flexibility in the template
             for (var i = 0, il = allColors.length; i < il; i++) {
                 allColors[i] = {
                     hex: allColors[i],
@@ -89,22 +111,35 @@ var golfHoleMaker = {
                 }
             };
 
+            // render the color/tile type selection form
             var compiledTemplate = _.template(this.templates.colorPicker);
             this.settings.colorPickerContainer.innerHTML = compiledTemplate({colors: allColors});
         } else {
             alert('image has too many colors');
         }
-
     },
 
+    /**
+     * Convert an integer into equivalent hexadecimal with leading zero
+     * @param {int} component
+     * @returns {string}
+     */
     componentToHex: function (component) {
         var hex = component.toString(16);
         return hex.length == 1 ? "0" + hex : hex;
     },
 
+    /**
+     * Create a hexadecimal color from input rgb values
+     * @param {int} r - red component of color to convert
+     * @param {int} g - green component of color to convert
+     * @param {int} b - blue component of color to convert
+     * @returns {string} color as hexadecimal
+     */
     rgbToHex: function (r, g, b) {
         return "#" + this.componentToHex(r) + this.componentToHex(g) + this.componentToHex(b);
     }
 };
 
+// start the tool
 golfHoleMaker.init();
